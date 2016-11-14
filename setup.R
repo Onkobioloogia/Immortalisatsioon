2d52e335e641a4235778c8f1662f2d59da7dc660
Taavi Päll <tapa741@gmail.com>
2016-11-14 10:26:41 +0200

    esilehe pilt

--- setup.R
+++ setup.R
@@ -3,7 +3,6 @@ library(slidifyLibraries)
 # setwd("~/Dropbox//Onkobioloogia")
 # author("Immortalisatsioon")
 
-setwd("~/Dropbox//Onkobioloogia//Immortalisatsioon")
 slidify("index.Rmd")
 browseURL("index.html")
 # publish("Immortalisatsioon", "tpall")
